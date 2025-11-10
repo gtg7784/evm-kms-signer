7c3180aa6cf31f40414bdb347a7f127793d29f7d
Taegeon Go (Alan) <tae.gun7784@gmail.com>
2025-11-10 15:28:01 +0900

    feat: implement custom error classes

---
+++ src/errors/index.ts
@@ -0,0 +1,34 @@
+export class KmsSignerError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'KmsSignerError'
+  }
+}
+
+export class DerParsingError extends KmsSignerError {
+  constructor(message: string) {
+    super(message)
+    this.name = 'DerParsingError'
+  }
+}
+
+export class KmsClientError extends KmsSignerError {
+  constructor(message: string, public readonly cause?: Error) {
+    super(message)
+    this.name = 'KmsClientError'
+  }
+}
+
+export class SignatureNormalizationError extends KmsSignerError {
+  constructor(message: string) {
+    super(message)
+    this.name = 'SignatureNormalizationError'
+  }
+}
+
+export class RecoveryIdCalculationError extends KmsSignerError {
+  constructor(message: string) {
+    super(message)
+    this.name = 'RecoveryIdCalculationError'
+  }
+}
